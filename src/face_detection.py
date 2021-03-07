3ac27032f1e80efaf50f538917e6e7e8cc90d684
sdoshi983 <sdoshi983@rku.ac.in>
2021-03-07 13:06:08 +0530

    initial and final commit

---
+++ src/face_detection.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+classifier = cv2.CascadeClassifier("../assets/haarcascade_frontalface_default.xml")
+
+while True:
+    isReturned, frame = cap.read()
+    if isReturned:
+        faces = classifier.detectMultiScale(frame)
+
+        for face in faces:
+            x, y, w, h = face
+            if(w > 100) and (h > 100):
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
+
+        cv2.imshow('My Face', frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
